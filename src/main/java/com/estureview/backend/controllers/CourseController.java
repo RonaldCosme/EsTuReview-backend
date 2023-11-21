d4b4641a1532879e776a356a7db5ff27d92c543c
Ronald <u201920185@upc.edu.pe>
2023-11-20 21:19:59 -0500

    cambios del llamados

--- src/main/java/com/estureview/backend/controllers/CourseController.java
+++ src/main/java/com/estureview/backend/controllers/CourseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.PostUpdate;
 import java.util.List;
-@CrossOrigin(origins = {"http://www.estureview.online"})
+@CrossOrigin(origins = "http://localhost:4200")
 
 
 @RestController
--- src/main/java/com/estureview/backend/controllers/UserController.java
+++ src/main/java/com/estureview/backend/controllers/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://www.estureview.online"})
+@CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
 @RequestMapping("/api")
